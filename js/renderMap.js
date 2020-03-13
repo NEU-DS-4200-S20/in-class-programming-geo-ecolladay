cd6e4f7a1436ca7a63872cec04e14c49720e7205
ecolladay <ecolladay@gmail.com>
2020-03-12 20:19:47 -0400

    Added rest of steps

--- js/renderMap.js
+++ js/renderMap.js
@@ -1,9 +1,24 @@
-var svgStates = d3.select("svg #states"); // (1)
+var svgStates = d3.select("svg #states"),
+    svgBoundary = d3.select("svg #boundary"),
+    states = {},
+    startYear = 1790,
+    currentYear = startYear;
 
-var projection = d3.geoAlbersUsa(); // (2)
+var width = window.innerWidth, // (1)
+  height = window.innerHeight;
+var projection = d3.geoAlbersUsa()
+  .translate([width / 2, height / 2]);  // (2)
 
 var path = d3.geoPath()
     .projection(projection);  // (3)
+
+d3.json("data/usa.json", function(error, boundary) {
+ svgBoundary.selectAll("path")
+     .data(boundary.features)
+     .enter()
+   .append("path")
+     .attr("d", path)
+});
 
 d3.json("data/states.json", function(error, topologies) {  // (4)
 
@@ -13,5 +28,14 @@ d3.json("data/states.json", function(error, topologies) {  // (4)
       .data(state.features)
       .enter()
     .append("path")
-      .attr("d", path);
+      .attr("d", path)
+      .style("fill", function(d, i) { 
+        console.log("d is ", d)
+        var name = d.properties.STATENAM.replace(" Territory", ""); 
+        return colors[name]; 
+      }).append("svg:title")
+      .text(function(d) { return d.properties.STATENAM; });
+
+
+      
 });
